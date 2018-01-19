4b2b413b759fb5b2944e610198745bce7f5bf502
Artem Govorov <artem.govorov@gmail.com>
2018-01-19 20:11:37 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,28 +1,11 @@
-const wallabyWebpack = require('wallaby-webpack');
-const webpackConfig = require('./webpack.config');
-const path = require('path');
-
-/*
-Also found https://github.com/wallabyjs/public/issues/469 which had the same window issue
-but now I have a cache issue appearing.
- */
-
 module.exports = function (wallaby) {
 
     // Adding this allows replace to find the relative path - but still not the alias
     process.env.NODE_ENV = 'testing';
     process.env.NODE_PATH += require('path').delimiter
-        + require('path').join(wallaby.projectCacheDir);
-
-    // Just using 'mocked' webpack config to mimic the alias
-    const webpackPostprocessor = wallabyWebpack({
-        resolve: {
-            alias: {
-                sheep : path.join(__dirname, './client/components/core/utils.js')
-            }
-        },
-        entry: './client/index.js'
-    });
+        + require('path').join(wallaby.projectCacheDir)
+        + require('path').delimiter
+        + require('path').join(wallaby.projectCacheDir, 'client/components');
 
 
     process.env.BABEL_ENV = 'test';
@@ -41,9 +24,11 @@ module.exports = function (wallaby) {
             '**/*.js?(*)': wallaby.compilers.babel()
         },
 
-        postprocessor: webpackPostprocessor,
-
-        bootstrap: function () {
+        bootstrap: function (wallaby) {
+          const moduleAlias = require('module-alias');
+          moduleAlias.addAliases({
+            'sheep': wallaby.projectCacheDir + '/client/components/core/utils.js'
+          });
           function setupWindow() {
             const jsdom = require('jsdom')
             const {JSDOM} = jsdom
@@ -64,6 +49,7 @@ module.exports = function (wallaby) {
           // adding this allows the window issue to go away
           // but creates a cannot read property of cache
           setupWindow()
+          require('testdouble').reset();
         },
 
         testFramework: 'mocha',
